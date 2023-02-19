1480746b6bcb11c27ee839ed72769255798b4def
Alex Nicholas <me@alexnicholas.dev>
2023-02-19 02:08:08 +0000

    get things working

--- server/seed.js
+++ server/seed.js
@@ -19,118 +19,114 @@ Array.prototype.randomElement = function () {
 
 console.log('This dbURI: ', dbURI);
 
-mongoose.connect(
-  dbURI,
-  {
+async function seed() {
+  mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-  },
-  (err, db) => {
-    if (err) return console.log(err);
-    console.log('Mongoose connected!');
-    db.dropDatabase()
-      .then(() => {
-        const facilitiesData = fs.readFileSync('./server/data/finalFacilities.json');
-        const facilitiesContent = JSON.parse(facilitiesData);
-        return Campground.create(
-          facilitiesContent.map((facility) => {
-            return {
-              ridbCampgroundId: facility.FacilityID,
-              ridbRecAreaId: facility.ParentRecAreaID,
-              name: facility.FacilityName,
-              description: facility.FacilityDescription,
-              phone: facility.FacilityPhone,
-              email: facility.FacilityEmail,
-              address1: facility.address1,
-              address2: facility.address2,
-              avgRating: 0,
-              city: facility.city,
-              state: facility.state,
-              accessible: facility.accessible,
-              longitude: facility.FacilityLongitude,
-              latitude: facility.FacilityLatitude,
-              attributes: facility.attributes,
-              media: facility.entityMedia,
-            };
-          })
-        );
-      })
-      .then((returnedCampgrounds) => {
-        console.log(`${returnedCampgrounds.length} campgrounds created. Happy camping!`);
-        const recAreaData = fs.readFileSync('./server/data/finalRecAreaData.json');
-        const recAreaContent = JSON.parse(recAreaData);
-        return RecArea.create(
-          recAreaContent.map((recArea) => ({
-            ridbRecAreaId: recArea.RecAreaID,
-            name: recArea.RecAreaName,
-            description: recArea.RecAreaDescription,
-            phone: recArea.RecAreaPhone,
-            email: recArea.RecAreaEmail,
-            address1: recArea.address1,
-            address2: recArea.address2,
-            website: recArea.website,
-            avgRating: 0,
-            city: recArea.city,
-            state: recArea.state,
-            longitude: recArea.RecAreaLongitude,
-            latitude: recArea.RecAreaLatitude,
-            lastUpdated: recArea.lastUpdated,
-            media: recArea.media,
-            campgrounds: [
-              ...returnedCampgrounds.filter(
-                (campground) => campground.ridbRecAreaId === recArea.RecAreaID
-              ),
-            ],
-          }))
-        );
-      })
-      .then((recAreas) => {
-        console.log(`${recAreas.length} rec areas created. Time to get rickety-recked.`);
-        return User.create([
-          {
-            username: 'alicnik',
-            email: 'alicnik@hotmail.com',
-            password: 'Alicnik123',
-            passwordConfirmation: 'Alicnik123',
-            firstName: 'Alex',
-            lastName: 'Nicholas',
-            isAdmin: true,
-            avatar: defaultAvatarArray.randomElement(),
-          },
-          {
-            username: 'ali_bhimani',
-            email: 'ali_bhimani21@aol.com',
-            password: 'Ali4President',
-            passwordConfirmation: 'Ali4President',
-            firstName: 'Ali',
-            lastName: 'Bhimani',
-            isAdmin: true,
-            avatar: defaultAvatarArray.randomElement(),
-          },
-          {
-            username: 'rich',
-            email: 'richardbekoe@gmail.com',
-            password: 'Rich1',
-            passwordConfirmation: 'Rich1',
-            firstName: 'Richard',
-            lastName: 'Bekoe',
-            isAdmin: true,
-            avatar: defaultAvatarArray.randomElement(),
-          },
-          {
-            username: 'doug',
-            email: 'doug@doug.com',
-            password: 'Douglas42',
-            passwordConfirmation: 'Douglas42',
-            firstName: 'Douglas',
-            lastName: 'Adams',
-            isAdmin: false,
-            avatar: defaultAvatarArray.randomElement(),
-          },
-        ]);
-      })
-      .catch((err) => console.log(err))
-      .finally(() => mongoose.connection.close());
-  }
-);
+  });
+
+  await mongoose.connection.dropDatabase();
+  console.log('Database dropped');
+
+  const facilities = JSON.parse(fs.readFileSync('./server/data/finalFacilities.json'));
+  const campgrounds = await Campground.create(
+    facilities.map((facility) => {
+      return {
+        ridbCampgroundId: facility.FacilityID,
+        ridbRecAreaId: facility.ParentRecAreaID,
+        name: facility.FacilityName,
+        description: facility.FacilityDescription,
+        phone: facility.FacilityPhone,
+        email: facility.FacilityEmail,
+        address1: facility.address1,
+        address2: facility.address2,
+        avgRating: 0,
+        city: facility.city,
+        state: facility.state,
+        accessible: facility.accessible,
+        longitude: facility.FacilityLongitude,
+        latitude: facility.FacilityLatitude,
+        attributes: facility.attributes,
+        media: facility.entityMedia,
+      };
+    })
+  );
+  console.log(`${campgrounds.length} campgrounds created. Happy camping!`);
+
+  const recAreaData = JSON.parse(fs.readFileSync('./server/data/finalRecAreaData.json'));
+  const recAreas = await RecArea.create(
+    recAreaData.map((recArea) => ({
+      ridbRecAreaId: recArea.RecAreaID,
+      name: recArea.RecAreaName,
+      description: recArea.RecAreaDescription,
+      phone: recArea.RecAreaPhone,
+      email: recArea.RecAreaEmail,
+      address1: recArea.address1,
+      address2: recArea.address2,
+      website: recArea.website,
+      avgRating: 0,
+      city: recArea.city,
+      state: recArea.state,
+      longitude: recArea.RecAreaLongitude,
+      latitude: recArea.RecAreaLatitude,
+      lastUpdated: recArea.lastUpdated,
+      media: recArea.media,
+      campgrounds: [
+        ...campgrounds.filter((campground) => campground.ridbRecAreaId === recArea.RecAreaID),
+      ],
+    }))
+  );
+  console.log(`${recAreas.length} rec areas created. Time to get rickety-recked.`);
+
+  await User.create([
+    {
+      username: 'alicnik',
+      email: 'alicnik@hotmail.com',
+      password: 'Alicnik123',
+      passwordConfirmation: 'Alicnik123',
+      firstName: 'Alex',
+      lastName: 'Nicholas',
+      isAdmin: true,
+      avatar: defaultAvatarArray.randomElement(),
+    },
+    {
+      username: 'ali_bhimani',
+      email: 'ali_bhimani21@aol.com',
+      password: 'Ali4President',
+      passwordConfirmation: 'Ali4President',
+      firstName: 'Ali',
+      lastName: 'Bhimani',
+      isAdmin: true,
+      avatar: defaultAvatarArray.randomElement(),
+    },
+    {
+      username: 'rich',
+      email: 'richardbekoe@gmail.com',
+      password: 'Rich1',
+      passwordConfirmation: 'Rich1',
+      firstName: 'Richard',
+      lastName: 'Bekoe',
+      isAdmin: true,
+      avatar: defaultAvatarArray.randomElement(),
+    },
+    {
+      username: 'doug',
+      email: 'doug@doug.com',
+      password: 'Douglas42',
+      passwordConfirmation: 'Douglas42',
+      firstName: 'Douglas',
+      lastName: 'Adams',
+      isAdmin: false,
+      avatar: defaultAvatarArray.randomElement(),
+    },
+  ]);
+
+  await mongoose.connection.close();
+}
+
+try {
+  seed();
+} catch (err) {
+  console.error(err);
+}
